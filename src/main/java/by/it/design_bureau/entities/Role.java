026939f5243afd01dfe2a254167a957e4279d0b7
Kingl88 <siarheitsarik@gmail.com>
2020-03-21 19:58:44 +0300

    test

--- src/main/java/by/it/design_bureau/entities/Role.java
+++ src/main/java/by/it/design_bureau/entities/Role.java
@@ -1,7 +1,8 @@
 package by.it.design_bureau.entities;
 
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-
+@NoArgsConstructor
 public enum Role implements GrantedAuthority {
     ROLE_USER, ROLE_ADMIN;
 
